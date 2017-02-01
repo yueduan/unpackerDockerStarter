fb79a5d25102a76fd5c7f150f56ebfbfe1dfaed7
yue <yue>
2017-01-31 21:19:48 -0500

    update scripts

--- restartTesting.sh
+++ restartTesting.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-
 DATE=`date +%Y-%m-%d`
 
 mkdir $DATE
 
+declare -A retVal1
+declare -A retVal2
 
-
-for id in $(sudo docker ps -a -q)
+for id in $(sudo docker ps -q)
 do 
 	echo "Retriving results from $id"
 	sudo docker exec $id /bin/sh -c "tar cf results_$id.tar.gz /results"
@@ -19,4 +19,36 @@ do
 	sudo docker exec $id /bin/sh -c "killall -9 adb; killall -9 python; killall -9 emulator64-arm"
 	sudo docker exec $id /bin/bash -c "cd /unpackerAutoTestingScripts;git pull"	
 	sudo docker exec $id /bin/bash -c "python /unpackerAutoTestingScripts/unpackerAutoTesting.py &" &
+done
+
+sleep 200
+
+while :
+do
+        for id in $(sudo docker ps -q)
+        do
+                retVal1[$id]=$(sudo docker exec $id /bin/sh -c "ls /results | wc -l")
+                echo "$id: ${retVal1[$id]}"
+        done
+
+        sleep 1000
+
+        for id in $(sudo docker ps -q)
+        do
+                retVal2[$id]=$(sudo docker exec $id /bin/sh -c "ls /results | wc -l")
+                echo "$id: ${retVal2[$id]}"
+        done
+
+        for id in $(sudo docker ps -q)
+        do
+                dif=$((${retVal2[$id]} - ${retVal1[$id]}))
+                if [ $dif -eq 0 ]
+                then
+                        echo "no progress for $id, restarting!"
+			sudo docker exec $id /bin/sh -c "killall -9 adb; killall -9 python; killall -9 emulator64-arm"
+			sudo docker exec $id /bin/bash -c "cd /unpackerAutoTestingScripts;git pull"
+			sudo docker exec $id /bin/bash -c "python /unpackerAutoTestingScripts/unpackerAutoTesting.py &" &			
+                fi
+        done
+
 done
